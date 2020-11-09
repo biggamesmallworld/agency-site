e4029739ba8bb020ee4dd7af02bb4062f40b1fe4
Will Nahmens <willnahmens@gmail.com>
2020-11-08 22:49:53 -0800

    changed api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ let host;
 if(process.env.NODE_ENV === "development") {
     host = "http://localhost:8080";
 } else {
-    host = "http://demoapi.willnahmens.com";
+    host = "http://demoapi.161.35.127.93";
 }
 
 
